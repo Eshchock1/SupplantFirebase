e1c75848969eae7f30a70d070d30d54d2947a929
Jeffrey Li <dr.cheddar@outlook.com>
2020-07-18 12:23:12 -0400

    Firebase functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,20 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+admin.initializeApp(functions.config().firebase);
+
+export const testAuth= functions.https.onRequest(async(req,res) => {
+    const token = req.headers.authorization;
+    if(token)
+    {
+        admin.auth().verifyIdToken(token).then(function(decodedToken)
+        {
+            res.json({result :`GOTTEM ${decodedToken} + ${token}`});
+        }).catch(err => {
+            res.json({result :`failed ${err} + ${token}`});
+        });
+    }    
+})
+
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
